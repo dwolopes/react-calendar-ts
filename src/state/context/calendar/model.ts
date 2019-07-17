f82bdaa4934f572bb0ac188eb26135df3585929e
Douglas <3440274-dwolopes1@users.noreply.gitlab.com>
2019-07-17 19:34:12 -0300

    feat: finishing api use and add base style, fonts and so on

--- src/state/context/calendar/model.ts
+++ src/state/context/calendar/model.ts
@@ -2,27 +2,44 @@ import * as repository from './repository';
 import { toast } from 'react-toastify';
 
 import { key } from '../../../utils/constants';
-import { dispatch } from 'rxjs/internal/observable/pairs';
 
 export const calendar = {
-    state: {
-        holidays: {},
-    },
-    reducers: {
-        holidays(state: any, payload: any){
-            return {
-                ...state, // precisa ?
-                holidays: {
-                    ...state.holidays,
-                    ...payload
-                }
-            }
-        },
-        clearStore() {
-            return {}
-        }
-    },
-    effects:(dispatch: any) =>({
-
-    })
-}
+	state: {
+		holidays: {},
+	},
+	reducers: {
+		holidays(state: any, payload: any) {
+			return {
+				...state,
+				holidays: {
+					...state.holidays,
+					...payload,
+				},
+			};
+		},
+		clearStore() {
+			return {};
+		},
+	},
+	effects: (dispatch: any) => ({
+		async getHolidaysAsync(year: number, getState: any) {
+			try {
+				const {
+					calendar: { holidays },
+				} = getState;
+				if (!holidays[year]) {
+					const response = await repository.getHolidays({
+						api_key: key,
+						country: 'BR',
+						year,
+					});
+					return dispatch.calendar.holidays({
+						[year]: response.data.holidays.holidays,
+					});
+				}
+			} catch (error) {
+				toast.error('Ops :( Algo deu errado tente novamente !');
+			}
+		},
+	}),
+};
